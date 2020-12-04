bb2e37692731c8b39a7416fb142aa1e08c9bfbff
slanycukr <hovadik@atlasgroup.cz>
2020-12-04 20:26:54 +0100

    Removing files from previous imported project

--- project/src/main/java/boulder_dash/Hittable.java
+++
@@ -1,9 +0,0 @@
-package boulder_dash;
-
-import java.io.Serializable;
-
-public interface Hittable extends Serializable {
-    void hitBy(VisibleEntity visibleEntity);
-    void addHitListener(HitListener listener);
-    void removeHitListener(HitListener listener);
-}
--- project/src/main/java/boulder_dash/SimulatedEntity.java
+++
@@ -1,7 +0,0 @@
-package boulder_dash;
-
-import java.io.Serializable;
-
-public interface SimulatedEntity extends Serializable {
-    public void simulate(int timeStep);
-}
